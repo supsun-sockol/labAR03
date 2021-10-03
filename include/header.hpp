9b12a8af710cbd47fe916eea4ac12f41eded30dc
Supsun <supsun-sockol@yandex.ru>
2021-10-03 22:54:40 +0300

    fix code style's errors

--- include/header.hpp
+++ include/header.hpp
@@ -14,10 +14,9 @@ class SharedPtr;
 template <typename T>
 class shared_ptr_control_block{
 public:
-    shared_ptr_control_block(){};
-    ~shared_ptr_control_block(){};
+    shared_ptr_control_block(){}
+    ~shared_ptr_control_block(){}
 private:
-
     std::map <T*, std::atomic_uint> table;
 
     friend class SharedPtr<T>;
@@ -30,31 +29,30 @@ template <typename T>
 class SharedPtr {
 public:
     SharedPtr(){
-        this->ptr=nullptr;
+        this->ptr = nullptr;
     }
-    SharedPtr(T* Ptr){
+    explicit SharedPtr(T* Ptr){
         typename std::map <T*, std::atomic_uint> :: iterator it;
         it = Shared_ptr_control_block<T>.table.find(Ptr);
         if (it == Shared_ptr_control_block<T>.table.end()){
-            it->second=1;
+            it->second = 1;
         } else{
             it->second++;
         }
-        this->ptr=Ptr;
+        this->ptr = Ptr;
     }
     SharedPtr(const SharedPtr& r){
         Shared_ptr_control_block<T>.table[r.ptr]++;
-        this->ptr=r.ptr;
+        this->ptr = r.ptr;
     }
     SharedPtr(SharedPtr&& r){
-        this->ptr=nullptr;
+        this->ptr =     nullptr;
         std::swap(this->ptr, r.ptr);
-
     }
     ~SharedPtr(){
-        if (this->ptr!=nullptr){
+        if (this->ptr != nullptr){
             Shared_ptr_control_block<T>.table[this->ptr]--;
-            if (Shared_ptr_control_block<T>.table[this->ptr]==0){
+            if (Shared_ptr_control_block<T>.table[this->ptr] == 0){
                 delete[] this->ptr;
             }
         }
@@ -63,7 +61,7 @@ public:
     auto operator=(SharedPtr&& r) -> SharedPtr&;
 
     // проверяет, указывает ли указатель на объект
-    operator bool() const{return this->ptr!=nullptr;}
+    operator bool() const{return this->ptr != nullptr;}
     auto operator*() const -> T&;
     auto operator->() const -> T*;
 
@@ -71,12 +69,12 @@ public:
     void reset();
     void reset(T* ptr);
     void swap(SharedPtr& r);
-    // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+    // возвращает количество объектов SharedPtr,
+    //которые ссылаются на тот же управляемый объект
     auto use_count() const -> size_t;
+
 private:
     T* ptr;
-
-    //friend class SharedPtr;
 };
 
 #endif // INCLUDE_HEADER_HPP_
