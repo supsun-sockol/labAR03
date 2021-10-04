005eb0eb713ab5a683d7fd772185176bf6779dc1
Supsun <supsun-sockol@yandex.ru>
2021-10-04 18:23:08 +0300

    add test

--- tests/test.cpp
+++ tests/test.cpp
@@ -13,7 +13,8 @@ TEST(SharedPtr, test1) {
     EXPECT_FALSE(A);
 }
 TEST(SharedPtr, test2) {
-    int *a = new int(5);
+    int *a = new int[1];
+    a[0] = 5;
     SharedPtr<int> A(a);
     EXPECT_EQ(A.use_count(), 1);
     EXPECT_EQ(*A, 5);
